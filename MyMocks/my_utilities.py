generate_spectrum: Fixed the NB normalization

MyMocks/my_utilities.py
@@ -479,8 +479,8 @@ def generate_spectrum( LINE , my_z , my_ew , my_flux_g , my_widths , my_noises ,
 
         noisy_spectrum = np.random.normal( 0.0 , Noise_in_my_w_Arr , len(w_Arr) )
 
+    g_w_Arr = gSDSS_data['lambda_Arr_f']
     '''
-    g_w_Arr = gSDSS_data[ 'lambda_Arr_f'       ]
     g_T_Arr = gSDSS_data[ 'Transmission_Arr_f' ]
     g_w     = gSDSS_data[ 'lambda_pivot'       ]
     g_FWHM  = gSDSS_data[ 'FWHM'               ]
@@ -494,10 +494,11 @@ def generate_spectrum( LINE , my_z , my_ew , my_flux_g , my_widths , my_noises ,
     '''
 
     ## Synthetic NB arround emission line##
-    snb_w_Arr = np.linspace(w_line - 72, w_line + 72, 50)
-    snb_T_Arr = np.ones(50)
-    snb_w     = w_line
-    snb_FWHM  = 145
+    snb_w_Arr = g_w_Arr
+    snb_T_Arr = np.zeros(snb_w_Arr.shape)
+    snb_w     = w_line * (1 + my_z)
+    snb_T_Arr[np.where(np.abs(snb_w_Arr - snb_w) < 72.)] = 1.
+    snb_FWHM  = 144.
 
     Noises_flux_snb = Synthetic_Photometry_measure_flux(
             w_Arr, noisy_spectrum, snb_w_Arr, snb_T_Arr, snb_w, snb_FWHM
@@ -505,7 +506,7 @@ def generate_spectrum( LINE , my_z , my_ew , my_flux_g , my_widths , my_noises ,
     source_flux_snb = Synthetic_Photometry_measure_flux(
             w_Arr, IGM_obs_continum, snb_w_Arr, snb_T_Arr, snb_w, snb_FWHM
             )
-    
+
     Continum_normalization = (my_flux_g - Noises_flux_snb) * 1. / (source_flux_snb)
     
     cont_around_line = (
@@ -518,7 +519,7 @@ def generate_spectrum( LINE , my_z , my_ew , my_flux_g , my_widths , my_noises ,
     obs_lya_line_Arr = np.absolute(obs_lya_line_Arr * my_flux_f)
 
     catalog_obs_spectrum_No_IGM = noisy_spectrum + obs_lya_line_Arr + Continum_normalization * obs_frame_spectrum
-    catalog_obs_spectrum        = noisy_spectrum + obs_lya_line_Arr + Continum_normalization * IGM_obs_continum
+    catalog_obs_spectrum = noisy_spectrum + obs_lya_line_Arr + Continum_normalization * IGM_obs_continum
     catalog_obs_spectrum_No_Line= noisy_spectrum + Continum_normalization * IGM_obs_continum
 
     return catalog_obs_spectrum , catalog_obs_spectrum_No_IGM, catalog_obs_spectrum_No_Line