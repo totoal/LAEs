af2ec0055a1217f1fa605307146d2727105db60a
totoal <ttalbertott@gmail.com>
2021-10-05 12:21:19 +0200

    Added commentary and switched simps to simpson

--- three_filter.py
+++ three_filter.py
@@ -1,14 +1,14 @@
 import numpy as np
-from scipy.integrate import simps
+from scipy.integrate import simpson
 
 '''
 alpha & beta: two auxiliary functions for the three-filter continuum estimate method
 '''
 def alpha(w_Arr, t):
-    return simps(w_Arr**2 * t, w_Arr) / simps(w_Arr*t, w_Arr)
+    return simpson(w_Arr**2 * t, w_Arr) / simpson(w_Arr*t, w_Arr)
 
 def beta(w_Arr, t, w_EL):
-    return np.interp(w_EL, w_Arr, t) * w_EL / simps(t*w_Arr, w_Arr)
+    return np.interp(w_EL, w_Arr, t) * w_EL / simpson(t*w_Arr, w_Arr)
 
 # Main function #
 def three_filter_method(NB, BB_LC, BB_LU,
@@ -16,6 +16,17 @@ def three_filter_method(NB, BB_LC, BB_LU,
                         t_NB, w_NB,
                         t_BB_LC, t_BB_LU, w_BB_LC, w_BB_LU,
                         w_EL):
+    '''
+    Classical 3-filter method that fits the spectral continuum to a straight line
+    INPUT arguments: flambda of NB, Line Contaminated BB, Line Uncontaminated BB and
+    their associated errors; transmission curves and central wavelenghts of these 3
+    filters.
+    RETURNS: Integrated line flux, A and B fit parameters with their errors.
+
+    Fit: flambda_cont = A * lambda + B
+
+    Details in Appendix A of Spinoso et al. 2020
+    '''
 
     a_LU = alpha(w_BB_LU, t_BB_LU)
     a_LC = alpha(w_BB_LC, t_BB_LC)
