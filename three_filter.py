3fbde78780cff71bc617b973abe1dc502ad13d94
totoal <ttalbertott@gmail.com>
2021-10-12 13:03:22 +0200

    Uncomplete commit

--- three_filter.py
+++ three_filter.py
@@ -10,7 +10,7 @@ def alpha(w_Arr, t):
 def beta(w_Arr, t, w_EL):
     return np.interp(w_EL, w_Arr, t) * w_EL / simpson(t*w_Arr, w_Arr)
 
-# Main function #
+## Main function
 def three_filter_method(NB, BB_LC, BB_LU,
                         NB_err, BB_LC_err, BB_LU_err,
                         t_NB, w_NB,
@@ -47,3 +47,34 @@ def three_filter_method(NB, BB_LC, BB_LU,
     B_err = (BB_LU_err**2 + a_LU**2 * A_err**2)**0.5
 
     return F_EL, A, B, A_err, B_err
+
+def NB_3fm(pm_data, pm_err, nb_c, tcurves, bb_dist=10, N_nb=4):
+    '''
+    Builds synthetic photometry for virtual broad bands made out of narrow bands which
+    serve as arguments for the three_filter_method function.
+
+    INPUTS: pm_data (60xN_sources), pm_err (60xN_sources), nb_c central NB, bb_dist
+    separation in Angstroms of the virtual BBs, N_nb number of NBs to make the virtual
+    BB.
+    '''
+    NB = pm_data[nb_c]
+    BB_LC = np.average(
+        pm_data[nb_c-N_nb : nb_c+N_nb+1], axis=1,
+        weights=pm_err[nb_c-N_nb : nb_c+N_nb+1] ** -2
+    )
+    BB_LU = np.average(
+        pm_data[nb_c-N_nb+bb_dist : nb_c+N_nb+1+bb_dist], axis=1,
+        weights=pm_err[nb_c-N_nb+bb_dist : nb_c+N_nb+1+bb_dist] ** -2
+    )
+    NB_err = pm_err[nb_c]
+    BB_LC_err = np.sum(pm_err[nb_c-N_nb : nb_c+N_nb+1] ** -2, axis=1) ** -0.5
+    BB_LU_err = np.sum(
+        pm_err[nb_c-N_nb+bb_dist : nb_c+N_nb+1+bb_dist] ** -2, axis=1) ** -0.5
+
+    w_Arr = tcurves['w'][nb_c]
+    t_NB = tcurves['t'][nb_c]
+    t_BB_LC = np.sum(
+        np.interp(w_Arr, tcurves['t'][nb_c-N_nb : nb_c+N_nb+1],
+        tcurves['w'][nb_c-N_nb : nb_c+N_nb+1]),
+        axis=1
+    )
