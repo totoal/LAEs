aad22cd53cebaef881cfecb2bf8e3d2db7bde055
totoal <ttalbertott@gmail.com>
2022-01-03 13:59:33 +0100

    Checkpoint

--- MyMocks/Make_LAE.py
+++ MyMocks/Make_LAE.py
@@ -18,7 +18,7 @@ def main(part):
     # w_lya = 1215.67
 
     ####    Mock parameters.
-    z_lya = [2, 5]
+    z_lya = [3, 4]
     obs_area = 50 # deg**2
 
     # Wavelength array where to evaluate the spectrum
