a7423f3164710bfcb7c4d1497fabfaacf1f34451
totoal <ttalbertott@gmail.com>
2022-05-14 11:52:48 +0200

    jeje

--- MyMocks/make_multiple_qso_altered.sh
+++ MyMocks/make_multiple_qso_altered.sh
@@ -5,7 +5,7 @@ declare -i N=1
 for i in {1..2}; do
   echo -e "\nROUND $i\n"
   for j in {1..12}; do
-    /home/alberto/cosmos/LAEs/MyMocks/Make_LAE.py $N&
+    /home/alberto/cosmos/LAEs/MyMocks/Make_QSO_altered.py $N&
     N=$((N + 1))
   done
   wait
