9c910913bb81ed760f9544b1b23793b2a645f037
totoal <ttalbertott@gmail.com>
2022-09-21 16:17:27 +0200

    Checkpoint

--- contaminants_hist.py
+++ contaminants_hist.py
@@ -1,4 +1,4 @@
-from minijpas_LF_and_puricomp import add_errors
+from minijpas_LF_and_puricomp import add_errors, search_lines
 from load_mocks import ensemble_mock
 from my_functions import *
 import matplotlib
@@ -23,51 +23,6 @@ w_central = central_wavelength()
 nb_fwhm_Arr = nb_fwhm(range(60))
 w_lya = 1215.67
 filter_tags = load_filter_tags()
-
-
-def search_LAEs(ew0_cut, ew_other, nb_min, nb_max, pm_flx, pm_err, zspec):
-    # Lya search
-    cont_est_lya, cont_err_lya = estimate_continuum(
-        pm_flx, pm_err, IGM_T_correct=True)
-    line = is_there_line(pm_flx, pm_err, cont_est_lya, cont_err_lya, ew0_cut)
-    lya_lines, lya_cont_lines, line_widths = identify_lines(
-        line, pm_flx, cont_est_lya, first=True, return_line_width=True
-    )
-    lya_lines = np.array(lya_lines)
-
-    # Other lines
-    cont_est_other, cont_err_other = estimate_continuum(
-        pm_flx, pm_err, IGM_T_correct=False)
-    line_other = is_there_line(pm_flx, pm_err, cont_est_other, cont_err_other,
-                               ew_other, obs=True)
-    other_lines = identify_lines(line_other, cont_est_other, pm_err)
-
-    # Compute z
-    z_Arr = np.zeros(N_sources)
-    z_Arr[np.where(np.array(lya_lines) != -1)] =\
-        z_NB(np.array(lya_cont_lines)[np.where(np.array(lya_lines) != -1)])
-
-    mag_min = 17
-    mag_max = 24
-
-    z_min = (w_central[nb_min] - nb_fwhm_Arr[nb_min] * 0.5) / w_lya - 1
-    z_max = (w_central[nb_max] + nb_fwhm_Arr[nb_max] * 0.5) / w_lya - 1
-    print(f'z interval: ({z_min:0.2f}, {z_max:0.2f})')
-
-    z_cut = (z_min < z_Arr) & (z_Arr < z_max)
-    mag_cut = (mag > mag_min) & (mag < mag_max)
-
-    snr = np.empty(N_sources)
-    for src in range(N_sources):
-        l = lya_lines[src]
-        snr[src] = pm_flx[l, src] / pm_err[l, src]
-
-    nice_lya_mask = z_cut & mag_cut & (snr > 6)
-    nice_lya = nice_lya_select(
-        lya_lines, other_lines, pm_flx, pm_err, cont_est_lya, z_Arr, mask=nice_lya_mask
-    )
-
-    return nice_lya, z_Arr, lya_lines
 
 
 def make_hist_plot(nice_lya, z_cut, lya_lines, ew0_cut, nb_min, nb_max):
@@ -148,7 +103,7 @@ if __name__ == '__main__':
     name_sf = f'LAE_12.5deg_z2-4.25_train_minijpas_0'
 
     pm_flx, pm_err, zspec, EW_lya, L_lya, is_qso, is_sf, is_gal, is_LAE, where_hiL, _ =\
-        ensemble_mock(name_qso, name_gal, name_sf, name_qso_bad, add_errs=False)
+        ensemble_mock(name_qso, name_gal, name_sf, name_qso_bad, add_errs=False, sf_frac=0.5)
 
     pm_flx, pm_err = add_errors(pm_flx, apply_err=True,
                                 survey_name='minijpasAEGIS001')
@@ -165,11 +120,22 @@ if __name__ == '__main__':
     for params_set in params:
         (ew0_cut, ew_other, nb_min, nb_max) = params_set
         print(params_set)
-        nice_lya, z_Arr, lya_lines =\
-            search_LAEs(ew0_cut, ew_other, nb_min, nb_max, pm_flx, pm_err, zspec)
+        # Estimate continuum, search lines
+        cont_est_lya, cont_err_lya, lya_lines, other_lines =\
+            search_lines(pm_flx, pm_err, ew0_cut, zspec, 'nb')
+
+        # Nice lya selection
+        nice_lya, z_Arr, lya_lines = nice_lya_select(lya_lines, other_lines, pm_flx,
+                                                     pm_err, cont_est_lya)
 
         z_min = (w_central[nb_min] - nb_fwhm_Arr[nb_min] * 0.5) / w_lya - 1
         z_max = (w_central[nb_max] + nb_fwhm_Arr[nb_max] * 0.5) / w_lya - 1
+        mag_min, mag_max = (17, 24)
 
         z_cut = (z_min < z_Arr) & (z_Arr < z_max)
+        mag_cut = (mag > mag_min) & (mag < mag_max)
+        nice_lya_mask = z_cut & mag_cut
+
+        nice_lya = nice_lya & nice_lya_mask
+
         make_hist_plot(nice_lya, z_cut, lya_lines, ew0_cut, nb_min, nb_max)
